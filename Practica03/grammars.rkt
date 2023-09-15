b43c2c6070c2be2dbd2b6cdeb007794278224dc8
Artgnz <arturognz@ciencias.unam.mx>
2023-09-14 19:43:04 -0600

    *** empty log message ***

--- Practica03/grammars.rkt
+++ Practica03/grammars.rkt
@@ -7,4 +7,12 @@
   [id (i symbol?)]
   [num (n number?)]
   [with (assigns (listof Binding?)) (body WAE?)]
+  [bool (b boolean?)]
+  [strinG (s string?)]
   [with* (assigns (listof Binding?)) (body WAE?)])
+
+(define (anD . args)
+  (foldl (lambda (a b) (and a b)) #t args))
+
+(define (oR . args)
+  (foldl (lambda (a b) (or a b)) #t args))
