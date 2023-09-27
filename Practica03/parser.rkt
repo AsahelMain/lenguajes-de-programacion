bcfba4fcffe34fc67a6796ce44c36aa45c2b609f
Arsenio <arsenik@badabing>
2023-09-26 20:16:31 -0600

    Correcciones de parentesis y de with*

--- Practica03/parser.rkt
+++ Practica03/parser.rkt
@@ -7,23 +7,20 @@
     [(number? s-exp) (num s-exp)]
     [(symbol? s-exp) (id s-exp)]
     [(boolean? s-exp) (bool s-exp)]
-    [(string? s-exp) (strinG s-exp)]
+    [(string? s-exp) (string s-exp)]
     [(Binding? s-exp) (binding parse(s-exp))]
     [(list? s-exp)
-     (let [head (car s-exp)]
+     (let [(head (car s-exp))]
        (case head
          [(sub1 add1 not zero? num? str? bool? str-length) (if (= (length (cdr s-exp)) 1) (op head (list parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) "espera 1 argumento. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
          [(modulo expt) (if (= (length (cdr s-exp)) 2) (op head (list (map parse(cdr s-exp)))) (error 'parse (string-append "La operacion " (symbol->string head) "espera 2 argumento. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
          [(+ - * / min max sqrt < > <= >= anD oR) (if (> (length (cdr s-exp)) 0) (op head (list (map parse(cdr s-exp)))) (error 'parse (string-append "La operacion " (symbol->string head) "espera mas de 0 argumentos. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
-         [(with) (let [repetido (symRepetido (flatten (second s-exp)))]
+         [(with) (let [(repetido (symRepetido (flatten (second s-exp))))]
                    (case repetido
                      [(null?) (with (parse(second s-exp)) (parse(third s-exp)))]
                      [else (error 'parse (string-append "El identificador " (symbol->string repetido) " esta declarado mas de una vez."))]))]
-         [(with*) (let [repetido (symRepetido (flatten (second s-exp)))]
-                   (case repetido
-                     [(null?) (with (parse(second s-exp)) (parse(third s-exp)))]
-                     [else (error 'parse (string-append "El identificador " (symbol->string repetido) " esta declarado mas de una vez."))]))]
-         [(Binding?) (list parse(head) parse(cdr-ls))]
+         [(with*) (with* (parse(second s-exp)) (parse(third s-exp)))]
+         [(Binding?) (list parse(head) parse(cdr s-exp))]
          ))
      ]))
 
@@ -33,4 +30,3 @@
                                            [(#f) (symRepetido (cdr ls))]
                                            [else car ls]))]
     [else (if (null? (cdr ls)) null (symRepetido (cdr ls)))]))
-
