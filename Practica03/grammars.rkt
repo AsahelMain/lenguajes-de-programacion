2494756d451d6d7533f080c65cef39a727a1cc25
Artgnz <arturognz@ciencias.unam.mx>
2023-09-29 19:43:05 -0600

    Agrega documentación.
    Documentación de ejercicios 1 y 2.

--- Practica03/grammars.rkt
+++ Practica03/grammars.rkt
@@ -3,17 +3,31 @@
 (define-type Binding
   [binding (id symbol?) (value WAE?)])
 
+#| Ejercicio 1 |#
+#| Gramática WAE extendida.
+|#
 (define-type WAE
   [num (n number?)]
   [id (i symbol?)]
   [bool (b boolean?)]
   [str (s string?)]
+  ;; Acepta cualquier procedimiento como operación.
   [op (f procedure?) (args (listof WAE?))]
   [with (assigns (listof Binding?)) (body WAE?)]
   [with* (assigns (listof Binding?)) (body WAE?)])
 
+#| Ejercicio 2 |#
+#| Función que recibe n argumentos y aplica al operador lógico
+   and sobre todos los argumentos.
+   any any ... any -> boolean
+|#
 (define (anD . args)
   (foldl (lambda (a b) (and a b)) #t args))
 
+#| Ejercicio 2 |#
+#| Función que recibe n argumentos y aplica al operador lógico
+   or sobre todos los argumentos.
+   any any ... any -> boolean
+|#
 (define (oR . args)
   (foldl (lambda (a b) (or a b)) #f args))
