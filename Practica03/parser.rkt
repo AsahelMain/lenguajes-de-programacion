475c58e313b82f2b430a3e4a01cdf72f44ce2457
Arsenik <arsenio@ciencias.unam.mx>
2023-09-29 18:39:25 -0600

    Correccion a bindingList

--- Practica03/parser.rkt
+++ Practica03/parser.rkt
@@ -14,7 +14,8 @@
          [(sub1 add1 not zero? num? str? bool? str-length sqrt) (if (= (length (cdr s-exp)) 1) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) " espera 1 argumento. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
          [(modulo expt) (if (= (length (cdr s-exp)) 2) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) " espera 2 argumentos. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
          [(+ - * / min max = < > <= >= anD oR) (if (> (length (cdr s-exp)) 0) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) " espera mas de 0 argumentos. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
-         [(with) (with (map parse-binding(bindingList (second s-exp) '())) (parse (third s-exp)))]))]))
+         [(with) (with (map(parse((bindingList (second s-exp) '() ) ))) (map (parse (third s-exp ))))]
+))]))
 
 (define (parse-binding ls)
   (case (length ls)
@@ -23,11 +24,11 @@
 
 (define (bindingList ls acc)
   (if (empty? ls)
-      (empty)
+      empty
       (let([head (first ls)])
         (if (member (car head) acc)
             (error 'parse (string-append "El identificador " (symbol->string (car head)) "esta declarado mas de una vez."))
-            (cons (list-to-binding head) (bindingList (car ls) (cons (car head) (acc))))))))
+            (cons (list-to-binding head) (bindingList (cdr ls) (cons (car head) acc)))))))
 
 (define (list-to-binding ls)
-  binding (first ls) (second ls))
+  (binding (first ls) (parse (second ls))))
