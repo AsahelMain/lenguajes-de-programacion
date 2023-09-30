55adb96436cea2993be4eeb9cf942ca52a04cdc5
Arsenio <arsenik@badabing>
2023-09-29 19:54:31 -0600

    Ajustes en parser.rkt con respecto a tests.rkt

--- Practica03/parser.rkt
+++ Practica03/parser.rkt
@@ -11,9 +11,9 @@
     [(list? s-exp)
      (let [(head (car s-exp))]
        (case head
-         [(sub1 add1 not zero? num? str? bool? str-length sqrt) (if (= (length (cdr s-exp)) 1) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) " espera 1 argumento. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
-         [(modulo expt) (if (= (length (cdr s-exp)) 2) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) " espera 2 argumentos. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
-         [(+ - * / min max = < > <= >= anD oR) (if (> (length (cdr s-exp)) 0) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operacion " (symbol->string head) " espera mas de 0 argumentos. Numero de argumentos recibidos: " (number->string (length (cdr s-exp))) ".") ))]
+         [(sub1 add1 not zero? num? str? bool? str-length sqrt) (if (= (length (cdr s-exp)) 1) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operación " (symbol->string head) " debe ser ejecutada con 1 argumentos.")))]
+         [(modulo expt) (if (= (length (cdr s-exp)) 2) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operación " (symbol->string head) " debe de ser ejecutada con 2 argumentos.")))]
+         [(+ - * / min max = < > <= >= anD oR) (if (> (length (cdr s-exp)) 0) (op (eval head) (map parse(cdr s-exp))) (error 'parse (string-append "La operación " (symbol->string head) " debe ser ejecutada con mas de 0 argumentos." )))]
          [(with) (with (bindingList (second s-exp) '()) (parse (third s-exp)))]
 ))]))
 
@@ -22,7 +22,7 @@
       empty
       (let([head (first ls)])
         (if (member (car head) acc)
-            (error 'parse (string-append "El identificador " (symbol->string (car head)) " esta declarado mas de una vez."))
+            (error 'parse (string-append "El identificador " (symbol->string (car head)) " está declarado más de una vez."))
             (cons (list-to-binding head) (bindingList (cdr ls) (cons (car head) acc)))))))
 
 (define (list-to-binding ls)
