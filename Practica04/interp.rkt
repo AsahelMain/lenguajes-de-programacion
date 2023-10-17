12d65cdd97ce083d28e5e2ed6a44895c24d5d806
Asahel <asahelmain@ciencias.unam.mx>
2023-10-16 21:49:01 -0600

    Completa funcion interp y funciones auxiliares

--- Practica04/interp.rkt
+++ Practica04/interp.rkt
@@ -1,10 +1,8 @@
 #lang plai
 
 (require "grammars.rkt")
-;(require "parser.rkt")
+(require "parser.rkt")
 (require "desugar.rkt")
-
-
 
 (define-type CFSBAE-Val
   [num-v (n number?)]
@@ -23,7 +21,17 @@
     [id (i) (lookup i env)]
     [bool (b) (bool-v b)]
     [strinG (s) (string-v s)]   
-    [op (f args) (error '0)]
+    [op (f args) (let* ([args-interpretados (map (lambda(x)(interp x env)) args)]
+                        [args-convertidos (map (lambda (x) (CFSBAE-Val->RacketValue x)) args-interpretados)])                   
+                   ;; Se revisa que los tipos de los argumentos dados sean validos.
+                   ;; En caso de no serlo se imprime el procedimiento con el tipo de argumento
+                   ;; esperado y el recibido.
+                   ;; Se decidió manejar los errores de esta forma debido a que
+                   ;; Racket lo hace de una forma similar.
+                   (when (verifica-argumentos f args-convertidos)
+                     (let ([resultado (apply f args-convertidos)])
+                         (RacketValue->CFSBAE-Val resultado))))
+                       ]
     [fun (params body) (closure-v params body env)]
     [app (f args) (let* ([fu (interp f env)]
                          [arg-val (map (lambda (x) (interp x env)) args)]
@@ -55,7 +63,56 @@
           (cons-env (first params) (first args) (extend-env (cdr params) (cdr args) env)))))
 
 
-(require racket/trace)
-(trace interp)
-(trace lookup)
-(trace extend-env)
+#|Función auxiliar de interp que verifica que los argumentos dados a
+  un procedimiento sean válidos, en caso de no serlos muestra un error.
+
+  verifica-argumentos: procedure (listof any) -> boolean
+|#
+(define (verifica-argumentos f args)
+  (let ([ops-con-num '(+ - / * min max expt sqrt sub1 add1 < > <= >= = zero?)]
+        [ops-con-int '(modulo )]
+        [ops-con-bool '(not anD oR)]
+        [ops-con-str '(string-length string?)])
+    (cond 
+      [(ormap (lambda (x) (eq? (translate x) f)) ops-con-num)
+       (if (andmap number? args)
+           #t
+           (error 'interp "~a: error: violación de contrato\nesperado: num\ndado: ~a"
+                  f (findf (lambda (x) (not (number? x))) args)))]
+      [(ormap (lambda (x) (eq? (translate x) f)) ops-con-int) (if (andmap integer? args)
+                                                             #t
+                                                             (error 'interp "~a: error: violación de contrato\nesperado: entero\ndado: ~a"
+                                                                   f (findf (lambda (x) (not (integer? x))) args)))]
+      [(ormap (lambda (x) (eq? (translate x) f)) ops-con-bool) (if (andmap boolean? args)
+                                                              #t
+                                                              (error 'interp "~a: error: violación de contrato\nesperado: bool\ndado: ~a"
+                                                                   f (findf (lambda (x) (not (boolean? x))) args)))]
+      [(ormap (lambda (x) (eq? (translate x) f)) ops-con-str) (if (andmap string? args)
+                                                             #t
+                                                             (error 'interp "~a: error: violación de contrato\nesperado: str\ndado: ~a"
+                                                                   f (findf (lambda (x) (not (string? x))) args)))]
+      [else #t])))
+
+
+;; CFSBAE-Val->RacketValue :: CFSBAE-Val -> number | boolean | string
+(define (CFSBAE-Val->RacketValue val)
+  (match val
+    [(num-v n) n]
+    [(bool-v b) b]
+    [(string-v s) s]
+    [_ "~val: error: no es un valor valido"]))
+
+
+;; RacketValue->CFSBAE-Val :: number | boolean | string -> CFSBAE-Val
+(define (RacketValue->CFSBAE-Val val)
+  (cond
+    [(number? val) (num-v val)]
+    [(boolean? val) (bool-v val)]
+    [(string? val) (string-v val)]
+    [else "~val: error: no es un valor valido"]))
+    
+
+;(require racket/trace)
+;(trace interp)
+;(trace lookup)
+;(trace extend-env)
--- Practica04/parser.rkt
+++ Practica04/parser.rkt
@@ -3,4 +3,44 @@
 (require "grammars.rkt")
 
 ;; parse :: s-exp -> CFWSBAE
-(define (parse s-exp))
+(define (parse s-exp) (error '0))
+
+
+
+(define (anD . args)
+  (foldl (lambda (a b) (and a b)) #t args))
+
+(define (oR . args)
+  (foldl (lambda (a b) (or a b)) #f args))
+
+
+#| Traduce los operadores implementados por su equivalente en Racket
+
+translate: procedure -> procedure|#
+(define (translate op)
+  (case op
+    [(+) +]
+    [(-) -]
+    [(-) -]
+    [(/) /]
+    [(*) *]
+    [(modulo) modulo]
+    [(min) min]
+    [(max) max]
+    [(expt) expt]
+    [(sqrt) sqrt]
+    [(sub1) sub1]
+    [(add1) add1]
+    [(<) <]
+    [(>) >]
+    [(<=) <=]
+    [(>=) >=]
+    [(=) =]
+    [(not) not]
+    [(and) anD]
+    [(or) oR]
+    [(zero?) zero?]
+    [(num?) number?]
+    [(str?) string?]
+    [(bool?) boolean?]
+    [(str-length) string-length]))
