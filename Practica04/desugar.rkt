670385b29f4090a29bbfd4a321b5dcfb21922951
Artgnz <arturognz@ciencias.unam.mx>
2023-10-23 01:03:23 -0600

    Adds documentation for desugar.

--- Practica04/desugar.rkt
+++ Practica04/desugar.rkt
@@ -2,7 +2,11 @@
 
 (require "grammars.rkt")
 (require "parser.rkt")
-
+#|Ejercicio 4
+  Función que recibe una expresión CFWSBAE y la transforma en una expresión
+  CFSBAE
+  desugar :: CFWSBAE -> CFSBAE
+|#
 (define (desugar expr)
   (type-case CFWSBAE expr
              [numS (n) (num n)]
@@ -11,10 +15,13 @@
              [strinGS (s) (strinG s)]
              [opS (f args) (op f (map desugar args))]
              [withS (bindings body)
+                    ;; La expresión se transforma en una aplicación de función.
                     (app (fun (map binding-id bindings) (desugar body))
                          (map (lambda (x) (desugar (binding-value x)))
                               bindings))]
              [with*S (bindings body)
+                     ;; La expresión se transforma en withs anidados y
+                     ;; se aplica desugar sobre la nueva expresión.
                      (desugar (with*S->withS expr))]
              [funS (params body) (fun params (desugar body))]
              [appS (f args) (app (desugar f) (map desugar args))]
@@ -22,12 +29,17 @@
                   (iF (desugar test-expr)
                       (desugar then-expr)
                       (desugar else-expr)))
+             ;; La expresión se transforma en ifs anidados.
              [conDS (conds else-expr)
                     (if (empty? conds)
                         (desugar else-expr)
                         (iF (desugar (condition-test-expr (first conds)))
                             (desugar (condition-then-expr (first conds)))
                             (desugar (conDS (rest conds) else-expr))))]))
+#|Función auxiliar transforma una expresión CFWSBAE with* a expresiones
+  with anidadas. Se usa para simplificar los procedimientos que involucran a with*.
+  with*->with: CFWSBAE -> CFWSBAE
+|#
 (define (with*S->withS expr)
   (let ([bindings (with*S-bindings expr)]
         [body (with*S-body expr)])
