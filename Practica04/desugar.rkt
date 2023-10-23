edf6798a46aeae3d5d59d04e404f0c179105c133
Artgnz <arturognz@ciencias.unam.mx>
2023-10-23 00:56:17 -0600

    Refactorizado desugar.

--- Practica04/desugar.rkt
+++ Practica04/desugar.rkt
@@ -1,9 +1,8 @@
 #lang plai
 
 (require "grammars.rkt")
-;; (require "parser.rkt")
+(require "parser.rkt")
 
-;; desugar :: CFWSBAE -> CFSBAE
 (define (desugar expr)
   (type-case CFWSBAE expr
              [numS (n) (num n)]
@@ -13,22 +12,22 @@
              [opS (f args) (op f (map desugar args))]
              [withS (bindings body)
                     (app (fun (map binding-id bindings) (desugar body))
-                         (map (lambda (x) (desugar (binding-value x))) bindings))]
+                         (map (lambda (x) (desugar (binding-value x)))
+                              bindings))]
              [with*S (bindings body)
                      (desugar (with*S->withS expr))]
              [funS (params body) (fun params (desugar body))]
              [appS (f args) (app (desugar f) (map desugar args))]
              (iFS (test-expr then-expr else-expr)
-                  (iF (desugar test-expr) (desugar then-expr) (desugar else-expr)))
+                  (iF (desugar test-expr)
+                      (desugar then-expr)
+                      (desugar else-expr)))
              [conDS (conds else-expr)
-                    (let* ([first-cond (first conds)]
-                           [first-test (condition-test-expr first-cond)]
-                           [first-then (condition-then-expr first-cond)])
-                      (iF (desugar first-test) (desugar first-then)
-                          (if (empty? (rest conds))
-                              (desugar else-expr)
-                              (desugar (conDS (rest conds) else-expr)))))]))
-
+                    (if (empty? conds)
+                        (desugar else-expr)
+                        (iF (desugar (condition-test-expr (first conds)))
+                            (desugar (condition-then-expr (first conds)))
+                            (desugar (conDS (rest conds) else-expr))))]))
 (define (with*S->withS expr)
   (let ([bindings (with*S-bindings expr)]
         [body (with*S-body expr)])
