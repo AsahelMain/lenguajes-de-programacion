063b4dd46ad1d37b23477c57087dff5a7181064d
Artgnz <arturognz@ciencias.unam.mx>
2023-12-01 23:18:54 -0600

    Agrega caso para op.

--- Practica06/verificador.rkt
+++ Practica06/verificador.rkt
@@ -3,7 +3,6 @@
 (require "grammars.rkt")
 (require "parser.rkt")
 
-(lookup 'y (gamma 'y (booleanT )(gamma 'x (numberT) (phi))))
 ;; TCFWSBAE x TypeContext -> Type
 (define (typeof expr ctx)
   (type-case TCFWSBAE expr
@@ -22,6 +21,14 @@
                              tipo-else))))]
              [conD (conditions else-expr)
                    (typeof (conD->iF expr) ctx)]
+             [op (f args)
+                 (let ([tipo-esperado (tipo-op f)]
+                       [tipos-conseguidos (map (lambda (x) (typeof x ctx)) args)])
+                   (if (andmap (lambda (x) (equal? x tipo-esperado)) tipos-conseguidos)
+                       tipo-esperado
+                       (let* ([tipo-distinto (obtener-tipo-distinto-param tipo-esperado tipos-conseguidos)]
+                              [param-distinto (first (filter (lambda (x) (equal? tipo-distinto (typeof x ctx))) args))])
+                         (error 'typeof "Error en el parametro ~v. Tipo esperado: ~v. Tipo dado: ~v" param-distinto tipo-esperado tipo-distinto))))]
              [fun (params return-type body)
                 (funT (append (map param-type params) (list return-type)))]
              [app (f params)
@@ -31,11 +38,14 @@
                          [params-types (map (lambda (x) (typeof x ctx)) params)])
                     (if (not (equal? expected-types params-types))
                         (error 'typeof "El tipo de los parámetros no es el esperado." )
-                        return-type)
-                    )]
+                        return-type))]
+             [with* (assigns body) (typeof (with*->with expr) ctx)]
              [else "TODO"]))
 
-
+  (define (obtener-tipo-distinto-param esperado lst)
+    (cond [(not (equal? (car lst) esperado)) (car lst)]
+          [else (obtener-tipo-distinto-param esperado (cdr lst))]))
+  
 (define (conD->iF expr)
   (let ([conditions (conD-conditions expr)]
         [else-expr (conD-else-expr expr)])
@@ -45,7 +55,20 @@
             (condition-then-expr (first conditions))
             (conD->iF (conD (rest conditions) else-expr))))))
 
-#|Función auxiliar de interp
+#|Función auxiliar de type transforma una expresión TCFWSBAE with* a expresiones
+with anidadas. Se usa para simplificar los procedimientos que involucran a with*.
+
+with*->with: TCFWSBAE -> TCFWSBAE
+|#
+(define (with*->with expr)
+  (let ([bindings (with*-bindings expr)]
+        [body (with*-body expr)])
+    (with (list (first bindings))
+          (if (empty? (rest bindings))
+              body
+              (with*->with (with* (rest bindings) body))))))
+
+#| Función auxiliar de type.
 Busca un símbolo en el contexto y devuelve su tipo.
 Si no lo encuentra regresa un error.
 lookup :: symbol x TypeContext -> Type
@@ -57,3 +80,52 @@ lookup :: symbol x TypeContext -> Type
                     (if (symbol=? sub-id id)
                         type
                         (lookup sub-id rest-context))]))
+#| Implementación propia de and
+anD: args->bool|#
+(define (anD . args)
+  (foldl (lambda (a b) (and a b)) #t args))
+
+#| Implementación propia de or
+oR: args->bool|#
+(define (oR . args)
+  (foldl (lambda (a b) (or a b)) #f args))
+
+(define (translate op)
+  (case op
+    [(+) +]
+    [(-) -]
+    [(-) -]
+    [(/) /]
+    [(*) *]
+    [(modulo) modulo]
+    [(min) min]
+    [(max) max]
+    [(expt) expt]
+    [(sqrt) sqrt]
+    [(sub1) sub1]
+    [(add1) add1]
+    [(<) <]
+    [(>) >]
+    [(<=) <=]
+    [(>=) >=]
+    [(=) =]
+    [(not) not]
+    [(and) anD]
+    [(or) oR]
+    [(zero?) zero?]
+    [(num?) number?]
+    [(str?) string?]
+    [(bool?) boolean?]
+    [(str-length) string-length]))
+
+(define (tipo-op op)
+  (let ([ops-con-num '(+ - / * min max expt sqrt sub1 add1 < > <= >= = zero? modulo)]
+        [ops-con-bool '(not and or)]
+        [ops-con-str '(string-length string?)])
+    (cond
+      [(ormap (lambda (x) (eq? (translate x) op)) ops-con-num)
+       (numberT)]
+      [(ormap (lambda (x) (eq? (translate x) op)) ops-con-bool)
+       (booleanT)]
+      [(ormap (lambda (x) (eq? (translate x) op)) ops-con-str)
+       (stringT)])))
